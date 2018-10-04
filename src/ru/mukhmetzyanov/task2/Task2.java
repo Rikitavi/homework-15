a8f1728dbaf1cc5928343075efc29f0bbd678a2d
Rikitavi <rikitavi777@yandex.ru>
2018-10-04 18:49:28 +0300

    Second task

--- src/ru/mukhmetzyanov/task2/Task2.java
+++ src/ru/mukhmetzyanov/task2/Task2.java
@@ -1,12 +1,52 @@
 package ru.mukhmetzyanov.task2;
+/**
+ * @author Ruslan Mukhametzyanov
+ */
 
 import java.io.File;
-import java.io.FilenameFilter;
 
 public class Task2 {
-
+    /**
+     * Папки указанные в File созданы заранее.ы
+     * @param args
+     */
     public static void main(String[] args) {
-        File toDelete = new File("/Users/ruslanmuhametzanov/For 15 homework/");
+
+        File toDelete = new File("/Users/ruslanmuhametzanov/For 15 homework");
+        deleteRecursive(toDelete);
+        if (toDelete.exists()){
+            System.out.println("Delete not successful");
+        }
+    }
+    
+    /**
+     *
+     * @param toDelete вызывает метод deleteRecursive, пока не удалятся все директории.
+     */
+    private static void deleteRecursive(File toDelete){
+        if (toDelete.isDirectory()){
+            File[] files = toDelete.listFiles();
+            System.out.println(files);
+            for (File file : files) {
+                deleteRecursive(file);
+            }
+        }
+        toDelete.delete();
+    }
+}
+
+//        File file = new File("/First/Second/test");
+//        System.out.println(file.getAbsolutePath());
+//        File[] files = file.listFiles();
+//        System.out.println(files);
+
+//        if (new File("/For 15 homework").delete() &&
+//            new File("/First").delete() &&
+//            new File("/Second").delete() &&
+//            new File("/test").delete()){
+//
+//            System.out.println("Удалены успешно");
+//        }
 
 
 
@@ -21,5 +61,3 @@ public class Task2 {
 //            }
 //        }
 //        toDelete.delete();
-    }
-}
